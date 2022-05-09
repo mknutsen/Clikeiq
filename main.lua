42ee71b5c0e9254988cc9860bcf2b4118b7b9e1b
Max Knutsen <mknutsen99@gmail.com>
2022-05-08 18:08:40 -0700

    added settings row

--- main.lua
+++ main.lua
@@ -14,33 +14,59 @@ import "CoreLibs/timer"
 import "CoreLibs/nineslice"
 
 
-local spread = 12 -- one octave
-local baseNoteSin = 50 -- near middle c
+spread = 12 -- one octave
+baseNoteSin = 60 -- near middle c
 
+sequence = playdate.sound.sequence.new()
 
-local synthSin = playdate.sound.synth.new()
+synthNoise = playdate.sound.synth.new()
+synthNoise:setWaveform(playdate.sound.kWaveNoise)
+trackNoise = playdate.sound.track.new()
+trackNoise:setInstrument(synthNoise)
+sequence:addTrack(trackNoise)
+
+synthSin = playdate.sound.synth.new()
 synthSin:setWaveform(playdate.sound.kWaveSine)
+trackSin = playdate.sound.track.new()
+trackSin:setInstrument(synthSin)
+sequence:addTrack(trackSin)
 
--- local channel = playdate.sound.channel.new()
+synthSaw = playdate.sound.synth.new()
+synthSaw:setWaveform(playdate.sound.kWaveSawtooth)
+trackSaw = playdate.sound.track.new()
+trackSaw:setInstrument(synthSaw)
+sequence:addTrack(trackSaw)
+
+synthTriangle = playdate.sound.synth.new()
+synthTriangle:setWaveform(playdate.sound.kWaveTriangle)
+trackTriangle = playdate.sound.track.new()
+trackTriangle:setInstrument(synthTriangle)
+sequence:addTrack(trackTriangle)
+
+
+
+-- channel = playdate.sound.channel.new()
 -- channel:addEffect(delayLine)
 -- channel:addSource(synthSin)
 
-local delayLenSec = 5
-local levelHalf = 0.5 -- theoretically level is 0-100 cant find confirmation tho
-local delayLine = playdate.sound.delayline.new(.5)
-delayLine:setMix(0.7)
+delayLenSec = 5
+levelHalf = 0.5 -- theoretically level is 0-100 cant find confirmation tho
+delayLine = playdate.sound.delayline.new(.3)
+delayLine:setMix(0.5)
 delayLine:setFeedback(0.1)
-delayLine:addTap(0.3)
+-- delayLine:addTap(0.3)
 delayLine:addTap(0.2)
 delayLine:addTap(0.1)
 playdate.sound.addEffect(delayLine)
 
-local sequence = playdate.sound.sequence.new()
-sequence:setTempo(2)
-local track = playdate.sound.track.new()
-sequence:addTrack(track)
-track:setInstrument(synthSin)
 
+filter = playdate.sound.twopolefilter.new(playdate.sound.kFilterLowPass)
+filter:setMix(.8)
+filter:setFrequency(600)
+filter:setResonance(0.5)
+playdate.sound.addEffect(filter)
+
+sequence:setTempo(2)
 sequence:setLoops(1, 8)
 sequence:play()
 
@@ -57,27 +83,65 @@ function dump(o)
    end
 end
 
+settingsRowNameTable = {}
+settingsRowNameTable[1] = "-"
+settingsRowNameTable[2] = "+"
+settingsRowNameTable[3] = "DEL"
+settingsRowNameTable[4] = "OD"
+settingsRowNameTable[5] = "1F"
+settingsRowNameTable[6] = "2F"
+settingsRowNameTable[7] = "BC"
+settingsRowNameTable[8] = "RING"
 
-local gfx = playdate.graphics
-local gridview = playdate.ui.gridview.new(44, 44)
--- gridview.backgroundImage = playdate.graphics.nineSlice.new('shadowbox', 4, 4, 45, 45)
+sectionNameTable = {}
+sectionNameTable[1] = "Noise"
+sectionNameTable[2] = "Sin"
+sectionNameTable[3] = "Saw"
+sectionNameTable[4] = "Triangle"
+
+baseNoteTable = {}
+baseNoteTable[1] = 50
+baseNoteTable[2] = 50
+baseNoteTable[3] = 50
+baseNoteTable[4] = 50
+
+function settingsButton(section, column)
+	local track = getTrack(section)
+	if column == 1 then -- octave down
+		baseNoteTable[section] -= 12
+	elseif column == 2 then -- octave up
+		baseNoteTable[section] += 12
+	elseif column == 3 then -- add delay
+	elseif column == 4 then -- add overdrive
+	elseif column == 5 then -- add unipolar filter
+	elseif column == 6 then -- add bipolar filter
+	elseif column == 7 then -- add bitcrush
+	elseif column == 8 then -- add ring mod
+		
+	end
+	
+end
+
+gridview = playdate.ui.gridview.new(44, 44)
 gridview:setNumberOfColumns(8)
--- gridview:setNumberOfRows(2, 4, 3, 5) -- number of sections is set automatically
-gridview:setNumberOfRows(1) -- number of sections is set automatically
--- gridview.backgroundImage = playdate.graphics.nineSlice.new('scrollbg', 20, 23, 92, 28)
+gridview:setNumberOfRows(2,2,2,2)
 gridview:setSectionHeaderHeight(24)
 gridview:setContentInset(1, 4, 1, 4)
 gridview:setCellPadding(4, 4, 4, 4)
 gridview.changeRowOnColumnWrap = false
 gridview:setScrollPosition(2,2)
 
--- local n = track:getNotes(1)
--- print(dump(n))
--- print(next(n) == nil)
-
 function stepActive(track, step)
 	local notes = track:getNotes(step)
 	return not (next(notes) == nil)
+end
+
+function getSectionName(sectionNumber)
+	return sectionNameTable[sectionNumber]
+end
+
+function getTrack(sectionNumber)
+	return sequence:getTrackAtIndex(sectionNumber)
 end
 
 function stepRemove(track, step)
@@ -97,8 +161,11 @@ end
 
 
 function gridview:drawCell(section, row, column, selected, x, y, width, height)
-	local isStepActive = stepActive(track, column)
-	-- stepActive = false
+	local isStepActive = false
+	if row == 2 then
+		local track = getTrack(section)
+		isStepActive = stepActive(track, column)
+	end
 	
 	local xMod = 4
 	local widthMod = -8
@@ -110,16 +177,19 @@ function gridview:drawCell(section, row, column, selected, x, y, width, height)
 		z = 3
 	end
 	if isStepActive then
-		gfx.fillCircleInRect(x + xMod, y + xMod, width + widthMod, height + widthMod, z)
+		playdate.graphics.fillCircleInRect(x + xMod, y + xMod, width + widthMod, height + widthMod, z)
 	else
-		gfx.drawCircleInRect(x + xMod, y + xMod, width + widthMod, height + widthMod, z)
+		playdate.graphics.drawCircleInRect(x + xMod, y + xMod, width + widthMod, height + widthMod, z)
 	end
 	local cellText = ""..row.."-"..column
-	gfx.drawTextInRect(cellText, x, y+14, width, 20, nil, nil, kTextAlignment.center)
+	if row == 1 then
+		cellText = settingsRowNameTable[column]
+	end
+	playdate.graphics.drawTextInRect(cellText, x, y+14, width, 20, nil, nil, kTextAlignment.center)
 end
 
 function gridview:drawSectionHeader(section, x, y, width, height)
-	gfx.drawText("*SECTION ".. section .. "*", x + 10, y + 8)
+	playdate.graphics.drawText(getSectionName(section) .. "*", x + 10, y + 8)
 end
 
 
@@ -127,7 +197,7 @@ end
 function playdate.update()
 	if gridview.needsDisplay == true then
 		playdate.graphics.clear()
-		gridview:drawInRect(0, 0, 400, 200)
+		gridview:drawInRect(0, 0, 400, 240)
 	end
 	
 	if playdate.buttonJustPressed(playdate.kButtonUp) then
@@ -148,13 +218,19 @@ function playdate.update()
 	
 	if playdate.buttonJustPressed(playdate.kButtonA) then
 		local section, row, column = gridview:getSelection()
-		local isStepActive = stepActive(track, column)
-		if isStepActive then
-			stepRemove(track, column)
-		else
-			local crank = playdate.getCrankPosition()
-			local crankModify =  (spread * (crank / 360))
-			track:addNote(column, baseNoteSin + crankModify, 1)
+		if row == 1 then
+			settingsButton(section, column)
+		elseif row == 2 then
+			local track = getTrack(section)
+			local isStepActive = stepActive(track, column)
+			if isStepActive then
+				stepRemove(track, column)
+			else
+				local crank = playdate.getCrankPosition()
+				local crankModify =  (spread * (crank / 360))
+				local baseNote = baseNoteTable[section]
+				track:addNote(column, baseNote + crankModify, 1)
+			end
 		end
 	end
 	-- listview:drawInRect(220, 20, 160, 210)
